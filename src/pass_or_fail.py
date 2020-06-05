dcbef14c8660cfb5656d3210cf301529caec7007
M4rqu1705 <m4rc05.dev@gmail.com>
2020-06-05 09:02:39 -0400

    Inclui un menu de Pass/Fail por si acaso es util. Se puede remover en el futuro

---
+++ src/pass_or_fail.py
@@ -0,0 +1,25 @@
+body = '''
+ 05/jun/2020            Recinto Universitario de Mayaguez             8:58 am
+                          MATRICULA - 2do Sem 2019-2020
+
+                        * Seleccion Modalidad Pass/Fail *
+
+       802-19-6220        MARCOS R PESANTE COLON                [0502]
+
+
+       Curso    Sec  Crs  P/F  Accion          Curso    Sec  Crs  P/F  Accion
+ 1.  QUIM 3132  050   3    P             7.  FISI 3173  086   1
+ 2.  QUIM 3134  06R   1                  8.
+ 3.  INGL 3212  101   3                  9.
+ 4.  MATE 3063  081   3                 10.
+ 5.  INGE 3016  020   3                 11.
+ 6.  FISI 3171  061   4                 12.
+
+
+
+
+
+
+
+     [Accion->  P=Marcar MODALIDAD P/F  E=Eliminar MODALIDAD P/F  F=Finalizar]
+'''
