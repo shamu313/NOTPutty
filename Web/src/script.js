2dee0b401a87af9b8d0978a63847f389ab5ff579
M4rqu1705 <m4rc05.dev@gmail.com>
2020-06-15 23:53:05 -0400

    Altas y bajas estan funcionando mejor, aunque hay detalles que se pueden mejorar
    TODO:
    - Hacer funcion para agregar cursos y corroborar que no hay conflicto ni nada
    - Posiblemente hacer menu adicional de seleccion de secciones (modo 4)
    - Hacer metodo para funcion que resetee las variables de buffer y demas a valores iniciales
    - Verificar en PuTTY real como es la dinamica de entrar cursos y secciones parciales o incorrectas
    - Hacer funcion de cambio de seccion

--- Web/src/script.js
+++ Web/src/script.js
@@ -19,10 +19,16 @@ const enrollment_dates = {
   "2do Sem": "23/nov/2019",
   "1er Verano": "4/may/2019",
 };
+const default_user_name = "Juan del Pueblo Rodríguez";
 
 var selected_term = "";
 var credits_selected = 0;
-var student_number = "";
+var student_number = "(802)00-0000";
+var course_list = {};
+var selected_courses = [
+  cursos_1er_verano_2020["MATE4009-01A"],
+  cursos_1er_verano_2020["INGE3016-001D"],
+];
 
 /***
  *       __ __    __               ____              __  _
@@ -31,6 +37,15 @@ var student_number = "";
  *    /_//_/\__/_/ .__/\__/_/   /_/  \_,_/_//_/\__/\__/_/\___/_//_/___/
  *              /_/
  */
+
+function centralize(string, width = 80) {
+  return " ".repeat((width - string.length) / 2) + string;
+}
+
+function pad_right(string, width) {
+  return String(string) + " ".repeat(width - String(string).length);
+}
+
 function header(title, long = true) {
   const today = new Date(Date.now());
   const year = today.getFullYear();
@@ -276,6 +291,7 @@ var main_menu_default = {
 var menu_2 = {
   lines: Array(4).fill("____________"),
   current_operation: 0,
+  user_name: "",
   buffer: "",
   header: function () {
     return header("E S T U D I A N T E", true);
@@ -283,6 +299,7 @@ var menu_2 = {
 
   body: function () {
     return `
+${this.user_name}
               --------------------------------------------------
               |                                                |
               |   Número Identificación       : ${
@@ -339,18 +356,19 @@ var menu_2 = {
       // Validate inputs and/or go on to next input
       switch (this.current_operation) {
         case 0:
-          // lines[0] = buffer + " " * (12 - len(buffer))
           this.lines[0] = this.buffer + " ".repeat(12 - this.buffer.length);
+          this.lines[0] = `<span class="underline">${this.lines[0]}</span>`;
 
           if (this.buffer.length === 9) {
             if (this.buffer.match(/^\d{9}$/g)) {
-              this.lines[0] = `(${this.buffer.slice(0, 3)})${this.buffer.slice(
+              this.lines[0] = `<span class="white-background">(${this.buffer.slice(0, 3)})${this.buffer.slice(
                 3,
                 5
-              )}-${this.buffer.slice(5, 10)}`;
+              )}-${this.buffer.slice(5, 10)}</span>`;
               this.buffer = "";
               this.current_operation = 1;
               student_number = this.lines[0].trim();
+              this.user_name = centralize(`<span class="white-background"> ${default_user_name.toUpperCase()} </span>`, 80 + 39);
             } else {
               this.buffer = "";
               this.lines[0] = "_".repeat(12);
@@ -363,7 +381,7 @@ var menu_2 = {
         case 2:
           if (this.buffer.length === 4) {
             if (this.buffer.match(/^\d{4}$/g)) {
-              this.lines[this.current_operation] = "****" + " ".repeat(8);
+              this.lines[this.current_operation] = "<span class='white-background'>****</span>" + " ".repeat(8);
               this.current_operation += 1;
               this.buffer = "";
             } else {
@@ -373,7 +391,7 @@ var menu_2 = {
           }
           break;
         case 3:
-          this.lines[3] = this.buffer + " ".repeat(12 - this.buffer.length);
+          this.lines[3] = "<span class='underline'>" + this.buffer + " ".repeat(12 - this.buffer.length) + "</span>";
 
           if (this.buffer.length === 8) {
             if (this.buffer.match(/^\d{8}$/g)) {
@@ -407,12 +425,14 @@ var menu_2 = {
   },
 };
 
+
 var term_selection = {
   header: function () {
     return header("S E L E C C I Ó N  D E  S E C C I O N E S", true);
   },
 
   body: "",
+  course_list: "",
 
   footer: `Indique Semestre  1=1er Sem,   2=2do Sem,   3=1er Verano o Verano Extendido
                   S=salir`,
@@ -429,18 +449,22 @@ var term_selection = {
         current_menu.refresh();
         break;
       case "1":
+        course_list = cursos_1er_sem_2020;
         selected_term = "1er Sem";
-        current_menu = course_selection;
+        current_menu = alta_bajas_cambio;
         current_menu.refresh();
         break;
       case "2":
+        course_list = cursos_2do_sem_2019;
         selected_term = "2do Sem";
-        current_menu = course_selection;
+        current_menu = alta_bajas_cambio;
         current_menu.refresh();
         break;
       case "3":
+        // Merge verano 1 and verano extendido courses
+        Object.assign(course_list, cursos_1er_verano_2020, cursos_verano_extendido_2020);
         selected_term = "1er Verano";
-        current_menu = course_selection;
+        current_menu = alta_bajas_cambio;
         current_menu.refresh();
         break;
       default:
@@ -449,46 +473,230 @@ var term_selection = {
   },
 };
 
-var course_selection = {
+var alta_bajas_cambio = {
+  user_name: "",
+  body_list: "",
+  right_panel: [],
+  mode: 0,
+  buffer: "",
+  choosing_section: false,
+  potential_courses: [],
+
   header: function () {
     return header(`SELECCIÓN DE SECCIONES ${selected_term} ESTUDIANTE`, false);
   },
 
   body: function () {
-    return ` ${student_number}  JUAN DEL PUEBLO RODRÍGUEZ       0000-0  00 ${enrollment_dates[selected_term]} Crs. TTY
+    return ` ${student_number}  ${this.user_name + " ".repeat(25 - this.user_name.length)}       0000-0  00 ${enrollment_dates[selected_term]} Crs. TTY
                                                            2:00 pm    ${credits_selected}   04
      C U R S O   Sección  Cr. Grado
- 1.  ICOM 4015     100H    4    S   █
- 2.  ICOM 4015 L   050L             █
- 3.
- 4.
- 5.
- 6.
- 7.
- 8.
- 9.
-10.
-11.
-12.`;
+${this.body_list}`;
   },
 
-  footer: `Indique:   A=Alta  B=Baja  C=Cambio  L=ListaEspera  H=HorEst  p=EvalúoPago
+  default_footer: `Indique:   A=Alta  B=Baja  C=Cambio  L=ListaEspera  H=HorEst  p=EvalúoPago
            M=MatEvalúo  F=HorEstGráfico  O=CódigoReservar S=Salir`,
 
+  action_footer: function () {
+    return `Abreviatura y número de curso  o  FIN                                  [${this.mode === 1 ? "Altas" : this.mode === 2 ? "Bajas" : "Cambio"}]
+<span class="underline">${pad_right(this.buffer, 10)}</span>`;
+  },
+
+  section_footer: function () {
+    return `Sección seleccionada, (PF3=(8)Secciones Disponibles  CAN=Regresar)
+<span class="underline">${pad_right(this.buffer, 10)}</span>`;
+
+  },
+
+  footer: "",
+
   refresh: function () {
+    this.user_name = default_user_name.toUpperCase();
+    this.body_list = "";
+
+    // Print list of selected courses
+    for (let i = 1; i <= 12; i++) {
+      this.body_list += centralize(i.toString(), 3) + ".  ";
+      if (selected_courses.length > i - 1) {
+        let course = selected_courses[i - 1];
+        this.body_list += `${pad_right(course["codificacion"], 10)}   ${pad_right(course["seccion"], 8)} ${pad_right(course["creditos"], 4)} ${course["grado"] === "Sub-graduados" ? "S" : "G"}   █`;
+      }
+
+      // Also potentially print right panel
+      if (this.right_panel[i - 1] !== undefined) {
+        let lines = this.body_list.split(/\n/g);
+        let last_line = lines.length - 1;
+        this.body_list += " ".repeat(41 - lines[last_line].length) + this.right_panel[i - 1];
+      }
+      this.body_list += '\n';
+    }
+
+
+
     this.handle_input(null);
   },
 
   handle_input: function (key) {
-    switch (key) {
-      case "S":
-      case "s":
-        current_menu = term_selection;
-        current_menu.refresh();
+
+    // if (key === null) {
+    //   display(this, absolute_height - 4);
+    // }
+
+    switch (this.mode) {
+      case 0:
+        switch (key) {
+          case "S":
+          case "s":
+            current_menu = term_selection;
+            current_menu.refresh();
+            break;
+          case "A":
+          case "a":
+            this.mode = 1;
+            this.refresh();
+            break;
+          case "B":
+          case "b":
+            this.mode = 2;
+            this.refresh();
+            break;
+          case "C":
+          case "c":
+            this.mode = 3;
+            this.refresh();
+            break;
+          default:
+            this.footer = this.default_footer;
+        }
         break;
-      default:
-        display(this, absolute_height - 4);
+      case 1:
+      case 2:
+      case 3:
+        if (this.choosing_section) {
+          this.footer = this.section_footer;
+        } else {
+          this.footer = this.action_footer;
+        }
+
+        if (typeof key === "string" && key !== "Enter") {
+          // If <Backspace> or <Delete> are pressed
+          if (key === "Backspace" || key === "Delete") {
+            this.buffer = this.buffer.slice(0, -1);
+          }
+          // Exclude any other keys like <AltGr> and such from being added to buffer
+          else if (key.length === 1 && this.buffer.length < 10) {
+            this.buffer += key;
+          }
+
+          this.refresh();
+        }
+
+        else if (typeof key === "string" && (key === "Enter" || key === "\n")) {
+          if (this.buffer.trim().toLowerCase() === "fin") {
+            this.mode = 0;
+            this.buffer = "";
+            this.right_panel = [];
+            this.potential_courses = [];
+            this.choosing_section = false;
+          }
+          // If the buffer looks like a course code and is doing "altas" and not choosing sections
+          else if (this.buffer.match(/[A-Za-z]{4}\d{4}/g) && this.mode === 1 && !this.choosing_section) {
+
+            // Filter potential courses
+            this.potential_courses = Object.keys(course_list).filter((key) => key.startsWith(this.buffer.trim().toUpperCase()));
+
+            // Prepare right panel if there are multiple potential courses
+            if (this.potential_courses.length > 0) {
+              this.right_panel = [
+                "SECCIONES DISPONIBLE CURSO: " + course_list[this.potential_courses[0]]["codificacion"],
+                ""
+              ];
+
+              // Add course sections to right panel
+              for (let i = 0; i < this.potential_courses.length; i++) {
+                last_index = this.right_panel.length - 1;
+                if (this.right_panel[last_index].length < 40) {
+                  this.right_panel[last_index] += course_list[this.potential_courses[i]]["seccion"] + "  ";
+                } else {
+                  this.right_panel.push(`${course_list[this.potential_courses[i]]["seccion"]}  `);
+                }
+              }
+
+              this.choosing_section = true;
+              this.footer = this.section_footer;
+              this.buffer = "";
+
+              // If there are no potential courses, warn the user
+            } else {
+              this.right_panel = [
+                "CURSO NO ENCONTRADO"
+              ];
+              this.buffer = "";
+            }
+          }
+          // If is choosing sections and there are potential courses
+          else if (this.choosing_section && this.potential_courses.length > 0 && this.buffer.length > 0) {
+
+            // If user wants to cancel ...
+            if (this.buffer.toLowerCase() === "can") {
+              // Reset everything
+              this.choosing_section = false;
+              this.mode = 0;
+              this.buffer = "";
+              this.footer = this.default_footer;
+              this.right_panel = [];
+
+            } else {
+
+              for (let i = 0; i < this.potential_courses.length; i++) {
+                if (course_list[this.potential_courses[i]]["seccion"] === this.buffer) {
+                  selected_courses.push(course_list[this.potential_courses[i]]);
+                  this.choosing_section = false;
+                  this.mode = 0;
+                  this.buffer = "";
+                  this.footer = this.default_footer;
+                  this.right_panel = []
+                  break;
+                }
+              }
+
+              // If no action was taken ...
+              if (this.choosing_section) {
+                this.buffer = "";
+                this.right_panel.push("Seleccione sección nuevamente");
+              }
+
+            }
+          }
+          // If buffer is a plausible number to delete by index and is doing "bajas"
+          else if ((this.buffer.match(/\d{1,2}/g) && 1 <= parseInt(this.buffer) && parseInt(this.buffer) <= selected_courses.length) && this.mode === 2) {
+            selected_courses.splice(parseInt(this.buffer) - 1, 1);
+            this.mode = 0;
+            this.buffer = "";
+            this.footer = this.default_footer;
+            this.right_panel = []
+          }
+          // If buffer looks like a course code and is doing "bajas"
+          else if (this.buffer.match(/[A-Za-z]{4}\d{4}/g) && this.mode === 2) {
+            for (let i = 0; i < selected_courses.length; i++) {
+              if (selected_courses[i]["codificacion"].startsWith(this.buffer.trim().toUpperCase())) {
+                selected_courses.splice(i, 1);
+                this.mode = 0;
+                this.buffer = "";
+                this.footer = this.default_footer;
+                this.right_panel = []
+                break;
+              }
+            }
+          }
+
+
+          this.refresh();
+        }
+
+        break;
+
     }
+    display(this, absolute_height - 4);
   },
 };
 
@@ -505,19 +713,19 @@ var menu_5 = {
   },
 
   body: `
-    MENU DESPLIEGUE:  (Ver otra informacion)
+MENU DESPLIEGUE: (Ver otra informacion)
 
-       1.  Evaluacion certificacion #4
-       2.  Curriculo
-       3.  Evaluo de facturacion de matricula
-       4.  Matricula
-       5.  Turno de seleccion de cursos/secciones o Examenes finales
-       6.  Horario de cursos disponibles en Matricula
-       7.  Titulo de cursos disponibles en Horario
-       8.  Horario de matricula grafico
-       9.  Evaluo de matricula e indicadores
+1.  Evaluacion certificacion #4
+2.  Curriculo
+3.  Evaluo de facturacion de matricula
+4.  Matricula
+5.  Turno de seleccion de cursos / secciones o Examenes finales
+6.  Horario de cursos disponibles en Matricula
+7.  Titulo de cursos disponibles en Horario
+8.  Horario de matricula grafico
+9.  Evaluo de matricula e indicadores
 
-       0.  Finalizar`,
+0.  Finalizar`,
 
   footer: "Opcion deseada:",
 
@@ -540,9 +748,9 @@ var menu_5 = {
 /***
  *       __ __             __                 __  ____          __       ___  ____        _      __
  *      / // /__ ___ _____/ /_  ___ ____  ___/ / / __/__  __ __/ / ___  / _/ / __/_______(_)__  / /_
- *     / _  / -_) _ `/ __/ __/ / _ `/ _ \/ _  / _\ \/ _ \/ // / / / _ \/ _/ _\ \/ __/ __/ / _ \/ __/
- *    /_//_/\__/\_,_/_/  \__/  \_,_/_//_/\_,_/ /___/\___/\_,_/_/  \___/_/  /___/\__/_/ /_/ .__/\__/
- *                                                                                      /_/
+ *     / _  / -_) _ `/ __ / __ / / _ `/ _ \/ _  / _\ \/ _ \/ / / / / / _ \/ _/ _\ \/ __/ __ / / _ \/ __/,
+ *    /_/ / / _ /\__ /\_, _ / _ /  \__ /  \_, _ / _//_/\_,_/ /___/\___/\_,_/_/  \___/_/  /___/\__/_/ /_/ .__/\__/
+ *       /_/
  */
 
 let ua = navigator.userAgent;
